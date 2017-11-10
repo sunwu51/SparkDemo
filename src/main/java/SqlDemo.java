28d4b1ea585154da617e0fa522a4ab01aead8868
Frank <sunwu51@126.com>
2017-11-10 19:21:43 +0800

    更新SparkSql的demo

--- src/main/java/SqlDemo.java
+++ src/main/java/SqlDemo.java
@@ -2,27 +2,49 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.sql.catalyst.expressions.aggregate.Max;
 import org.apache.spark.sql.hive.HiveContext;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.Row;
+
+import java.io.File;
+
+import static org.apache.spark.sql.functions.*;
+
 
 /**
  * Created by Frank on 2017/8/6.
  */
 public class SqlDemo {
-    static SparkConf conf = new SparkConf().setAppName("app").setMaster("local");
-    static JavaSparkContext sc = new JavaSparkContext(conf);
-    public static void main(String[] a){
-        HiveContext hc=new HiveContext(sc);
-        Dataset ds = hc.jsonFile("hdfs://wy:9000/testpath/test.txt/1.json");
-        ds.registerTempTable("user");
-        JavaRDD rdd=hc.sql("select name from user").toJavaRDD();
-        JavaRDD<String> rddname = rdd.map(new Function<Row,String>() {
-            public String call(Row o) throws Exception {
-                return o.getString(0);
-            }
-        });
 
-        System.out.println(rddname.collect());
+    public static void main(String[] a){
+
+        //创建自己的仓库存储目录
+//        String warehouseLocation = new File("/spark-hive-warehouse").getAbsolutePath();
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Spark Hive")
+//                .config("spark.sql.warehouse.dir", warehouseLocation)
+//                .enableHiveSupport()
+                .master("local")
+                .getOrCreate();
+
+        //enableHiveSupport
+        //可以直接运行hql
+//        spark.sql("CREATE TABLE test(id int,name string,age int)ROW FORMAT DELIMITED FIELDS TERMINATED BY '|'");
+//        spark.sql("load data local inpath '/root/2.txt' into table test");
+//        spark.sql("select * from test").show();
+//
+        Dataset<Row> df = spark.read().json("1.json");
+
+        df.show();
+        df.createOrReplaceTempView("people");
+
+        Dataset<Row> sqlDF = spark.sql("SELECT * FROM people where name like '%Fra%'");
+
+        sqlDF.show();
+
     }
 }
